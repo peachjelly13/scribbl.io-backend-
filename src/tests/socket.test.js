e00cdb0e23322b6503d5aa72535e9d91a5058ebd
peachjelly13 <ananyasharma112233441234@gmail.com>
2025-04-12 23:57:24 +0530

    adding test cases

--- src/tests/socket.test.js
+++ src/tests/socket.test.js
@@ -1,8 +1,39 @@
 import http from 'http';
 import express from 'express';
-import { setupSocket } from '../webSockets/webSockets.connection';
-import {io as Client} from 'socket.io-client'
-import { Server } from 'socket.io';
+import { setupSocket } from '../webSockets/webSockets.connection.js'; // Assuming correct path
+import { io as Client } from 'socket.io-client';
 
+let server, clientSocket;
 
+beforeAll((done) => {
+  const app = express();
+  server = http.createServer(app);
+  setupSocket(server);
+  server.listen(() => {
+    const port = server.address().port;
+    clientSocket = Client(`http://localhost:${port}`);
+    clientSocket.on("connect", () => {
+      console.log("Client connected to server");
+      done();
+    });
+  });
+});
 
+afterAll(() => {
+  clientSocket.close();
+  server.close();
+});
+
+test('should connect and emit connectUser', (done) => {
+  clientSocket.emit('connectUser', {
+    username: 'Timmy',
+    avatar: 'avatar.png',
+    isPrivate: false,
+    privateRoomId: null
+  });
+
+  setTimeout(() => {
+    expect(true).toBe(true);
+    done();
+  }, 100);
+});
